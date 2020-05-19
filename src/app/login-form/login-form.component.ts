bd24e8e92756dead1c99e2de3e0fe5793c31655d
JC <thompsonrell@gmail.com>
2020-05-19 19:56:15 -0400

    signin and signup form

---
+++ src/app/login-form/login-form.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.css']
+})
+export class LoginFormComponent implements OnInit {
+
+  constructor(private router: Router, private authService: AuthService) { }
+
+  ngOnInit(): void {
+  }
+
+  loginUser(e) {
+    let email = e.target.elements[0].value;
+    let password = e.target.elements[1].value;
+    console.log(email, password);
+    // return false;
+    if (email == 'admin@gmail.com' && password == '12345') {
+      this.authService.login();
+      this.router.navigate(['home']);
+    }
+
+  }
+
+  onLogin() {
+    this.authService.login();
+  }
+
+  onLogout() {
+    this.authService.logout();
+  }
+
+}
