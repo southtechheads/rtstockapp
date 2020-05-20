9de8bb142de7a293b09ccc72e83247af3c107864
afbrown1216 <fay.ashbro@gmail.com>
2020-05-20 14:55:41 -0400

    Stock Service changes

--- src/app/stock.service.ts
+++ src/app/stock.service.ts
@@ -12,6 +12,9 @@ export class StockService {
   quoteURL: string = '/quote?symbol=';
   firebaseURL: string;
   symbol: string;
+  profile: any;
+  symbols:any;
+  topMovers:any[] = ['MSFT', 'AAPL', 'AMZN', 'GOOGL','FB','INTL']
   // companynewsURL '/company-news?symbol=AAPL&from=2020-04-30&to=2020-05-01'
   // earnings '/calendar/earnings?from=2010-01-01&to=2020-03-15&symbol=AAPL'
 
@@ -20,9 +23,43 @@ export class StockService {
 
 // intial SignIn calls 
   getSymbols(): Observable<Symbol[]> {
-    return this.http.get<Symbol[]>(`${this.stockURL}${this.symbolURL}`);
+    this.symbols = this.http.get<Symbol[]>(`${this.stockURL}${this.symbolURL}`)
+    if (this.symbols){
+      console.log(this.symbols)
+    } 
+    return this.symbols;  
   }
+
+  getProfile(input:string){
+   this.profile = this.http.get(`${this.stockURL}${this.companyProfileURL}${input}`);
+   type Profile = {
+     country: string 
+     name: string 
+     logo: string 
+     market: number
+     url: string
+   }
+   let newprofile = <Profile>{
+    country:  this.profile.country, 
+    name: this.profile.name,
+    logo: this.profile.logo,
+    market: this.profile.marketCapilization,
+    url:  this.profile.weburl
+   }
+   return newprofile
+  }
+  
+
+
+  
 }
+   
+
+
+
+  
+
+
 
 // functions we want
 //SignIn()
