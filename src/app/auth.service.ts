b23aa8e0319da90e7c43145fa3ad34e568e43e5e
Marnicus <leemarcus030@gmail.com>
2020-05-19 23:42:25 -0400

    Added to auth service
    Auth service now saves to database and localstorage.

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,39 +1,92 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
 
-
+@Injectable({
+  providedIn: 'root',
+})
 export class AuthService {
+  private baseURL: string = 'https://finnhub.io/api/v1/';
+  private symbolURL: string = '/stock/symbol?exchange=US';
+  private companyProfileURL: string = '/stock/profile2?symbol=';
+  private quoteURL: string = '/quote?symbol=';
+  private firebaseURL: string;
+  private symbol: string;
 
-    // private isUserLoggedIn;
-    // private username;
+  constructor(private auth: AngularFireAuth) {}
+  // private isUserLoggedIn;
+  // private username;
 
-    // constructor() {
-    //     this.isUserLoggedIn = false
-    // }
+  // constructor() {
+  //     this.isUserLoggedIn = false
+  // }
 
-    // setUserLoggedIn() {
-    //     this.isUserLoggedIn = true;
-    // }
+  // setUserLoggedIn() {
+  //     this.isUserLoggedIn = true;
+  // }
 
-    // getUserLoggedIn() {
-    //     return this.isUserLoggedIn;
-    // }
-    loggedIn: boolean;
+  // getUserLoggedIn() {
+  //     return this.isUserLoggedIn;
+  // }
+  loggedIn: boolean;
 
-    isAuthenticated() {
-        const promise = new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve(this.loggedIn)
-            }, 1000)
-        })
-        return promise;
+  async firebaseSignIn(email, password) {
+    const firebaseAuthSignIn = await this.auth
+      .signInWithEmailAndPassword(email, password)
+      .catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+      });
+
+    if (firebaseAuthSignIn) {
+      console.log(firebaseAuthSignIn.user.uid);
+      const userId = firebaseAuthSignIn.user.uid;
+        //sa
+      await localStorage.setItem('stockapp:uid', JSON.stringify(userId));
+
+      return true;
+    }
+    return false;
+  }
+
+  async firebaseSignUp(email, password) {
+    const firebaseAuthSignUp = await this.auth
+      .createUserWithEmailAndPassword(email, password)
+      .catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+      });
+
+    const getUserData = firebaseAuthSignUp;
+
+    console.log('test sign up method: ', getUserData);
+    if (getUserData) {
+      return true;
     }
 
-    login() {
-        this.loggedIn = true;
-    }
+    return false;
+  }
 
-    logout() {
-        this.loggedIn = false;
-    }
+  isAuthenticated() {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.loggedIn);
+      }, 1000);
+    });
+    return promise;
+  }
 
+  login() {
+    this.loggedIn = true;
+  }
 
+  logout() {
+    this.loggedIn = false;
+  }
 }
